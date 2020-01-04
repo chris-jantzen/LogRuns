45b79053792a195d0d0272db2740ac1acab661f0
Chris Jantzen <jantzeca@mail.uc.edu>
2020-01-04 14:47:56 -0500

    remove unused cors stuff

--- server/server.ts
+++ server/server.ts
@@ -29,14 +29,6 @@ class App {
     this.app.use(morgan('dev'));
     this.app.use(express.json());
     this.app.use(cors());
-    // this.app.use((req, res, next) => {
-    //   res.header('Access-Control-Allow-Origin', '*');
-    //   res.header(
-    //     'Access-Control-Allow-Headers',
-    //     'Origin, X-Requested-With, Content-Type, Accept'
-    //   );
-    //   next();
-    // });
     await this.importControllers();
 
     try {
